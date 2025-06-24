6f0beb961ef6e7cd8ef8496e98076405364c7ba1
sumit-codes4123 <sumitgarg4123@gmail.com>
2025-06-24 18:04:12 +0530

    Time: 0 ms (100%), Space: 28.3 MB (6.09%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int c;
+        map<int, int> mpp;
+        for (int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+        }
+        for (auto& it : mpp) {
+            if (it.second > n / 2) {
+                c = it.first; // Fix: assign the element, not frequency
+                break;        // Optional: break early
+            }
+        }
+        return c;
+    }
+};
