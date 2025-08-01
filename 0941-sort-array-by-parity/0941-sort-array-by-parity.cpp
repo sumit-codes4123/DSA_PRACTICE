34903fca9c05bd33600066b8751973d9866569e6
sumit-codes4123 <sumitgarg4123@gmail.com>
2025-08-01 13:07:09 +0530

    Time: 4 ms (8.17%), Space: 20.6 MB (37.63%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int even = 0, odd = n-1 ;
+        for(int i=0;i<n;i++) {
+            if (nums[i] % 2 == 0) {
+                ans[even++] = nums[i];
+            }else{
+                ans[odd--]=nums[i];
+            }
+        }return ans;
+    }
+};
