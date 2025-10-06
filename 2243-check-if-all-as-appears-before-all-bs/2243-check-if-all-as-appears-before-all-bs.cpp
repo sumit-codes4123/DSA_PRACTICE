7974a27a16f5264fdccbac704a6264ee4dafefd1
sumit-codes4123 <sumitgarg4123@gmail.com>
2025-10-06 23:45:57 +0530

    Time: 0 ms (100%), Space: 8.4 MB (15.17%) - LeetHub

---
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        for(int i=0;i+1<s.size();i++){
+            if(s[i]=='b'&&s[i+1]=='a')return false;
+        }return true;
+    }
+};
