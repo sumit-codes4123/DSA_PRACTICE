ff03ad673b882e0a0f592a045140b8d613e0d74b
sumit-codes4123 <sumitgarg4123@gmail.com>
2025-10-14 10:13:46 +0530

    Time: 0 ms (100%), Space: 9.9 MB (5.8%) - LeetHub

---
+++ 3019-furthest-point-from-origin/3019-furthest-point-from-origin.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        unordered_map<char, int> count;
+        for (auto c : moves)
+            count[c]++;
+        return max(count['L'], count['R']) - min(count['L'], count['R']) +
+               count['_'];
+    }
+};
