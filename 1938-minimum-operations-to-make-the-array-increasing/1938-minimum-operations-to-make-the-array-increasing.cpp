6a517fe16ae64c234e00e2b303064fd4acefdaa2
sumit-codes4123 <sumitgarg4123@gmail.com>
2025-11-07 22:10:39 +0530

    Time: 12 ms (39.37%), Space: 19.4 MB (83.94%) - LeetHub

---
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int cnt=0;
+        for(int i=0;i+1<nums.size();i++){
+            if(nums[i+1]<=nums[i]){
+              int diff=nums[i]-nums[i+1];
+              cnt+=diff+1;
+              nums[i+1]+=diff+1;
+            }
+        }return cnt;
+    }
+};
