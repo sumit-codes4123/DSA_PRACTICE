bc80d58f4b5774da69806bb41925457bdf8514ba
sumit-codes4123 <sumitgarg4123@gmail.com>
2025-11-08 14:09:14 +0530

    Time: 0 ms (100%), Space: 22.9 MB (53.94%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+       int n=ratings.size();
+       vector<int>candy(n,1);
+       for(int i=1;i<n;i++){
+        if(ratings[i]>ratings [i-1])
+        candy[i]=candy[i-1]+1;
+       } 
+       int cnt = candy[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1])
+                candy[i] = max(candy[i], candy[i + 1] + 1);
+            cnt += candy[i];
+        }
+        return cnt;
+    }
+};
