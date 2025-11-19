95c8e92d5ca19cdd5d287fb464fdbfe9b1247c96
sumit-codes4123 <sumitgarg4123@gmail.com>
2025-11-19 22:43:06 +0530

    Time: 24 ms (28.87%), Space: 28.4 MB (74.03%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string removeStars(string s) {
+       string res="";
+        for (char c : s) {
+            if (c != '*')
+                res.push_back(c);
+            else res.pop_back();
+        }
+        return res;
+    }
+};
