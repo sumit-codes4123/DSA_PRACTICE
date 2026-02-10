d3630e8a1a5804c9e9dfab138c67bc0bd1238b29
sumit-codes4123 <sumitgarg4123@gmail.com>
2026-02-10 08:17:04 +0530

    Time: 1 ms (96.07%), Space: 190.7 MB (10.04%) - LeetHub

---
+++ 3255-find-the-power-of-k-size-subarrays-ii/3255-find-the-power-of-k-size-subarrays-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>dp(n+1);
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]-nums[i-1]==1) dp[i]=dp[i-1]+1;
+            else dp[i]=1;
+        }vector<int>ans;
+        for(int i=k-1;i<n;i++){
+            if(dp[i]>=k)ans.push_back(nums[i]);
+            else ans.push_back(-1);
+        }return ans;
+    }
+};
