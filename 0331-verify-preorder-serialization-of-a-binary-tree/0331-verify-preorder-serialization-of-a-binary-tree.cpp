59d199ca2f6346571b9ab9c8939e845f0740a4ac
sumit-codes4123 <sumitgarg4123@gmail.com>
2026-02-22 00:16:56 +0530

    Time: 0 ms (100%), Space: 9.8 MB (59.55%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        if(preorder.empty()) return false;
+        int c=1;
+        for(int i=0;i<preorder.size();i++){
+            if(preorder[i]==',') continue;
+            c--;
+            if(c<0)return false;
+            if(preorder[i]!='#'){
+                while(i<preorder.size()&&preorder[i]!=',')
+               i++;
+                c+=2;
+            }
+        }if(c==0)return true;
+        return false;
+    }
+};
