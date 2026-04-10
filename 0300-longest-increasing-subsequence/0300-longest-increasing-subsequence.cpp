c57379e8fbd8e6d0c9f5d6e451fb0f4a2e11dc98
sumit-codes4123 <sumitgarg4123@gmail.com>
2026-04-10 23:52:02 +0530

    Time: 276 ms (28.69%), Space: 296.6 MB (11.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    int solve(int i,int p,vector<int>&nums,vector<vector<int>>&dp){
-        if(i==nums.size())return 0 ;
-        if(dp[i][p+1]!=-1)return dp[i][p+1];
-        int exclude=solve(i+1,p,nums,dp);
-
-        int include=0;
-        if(p==-1||nums[i]>nums[p]){
-            include=1+solve(i+1,i,nums,dp);
-        }
-        return dp[i][p+1]= max(include,exclude);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-         return solve(0,-1,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=n;j++){
-        //         if(nums[i]>=nums[j]){
-        //             dp[i][j]=1+dp[i-1][j-1];
-        //         }
-        //         else{
-        //             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-        //         }
-        //     }
-        // }
-        // return dp[0][0];
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+               int len = dp[i+ 1][j+ 1];
+                if (j== -1 || nums[i] > nums[j]) len = max(len, 1 + dp[i+ 1][i+ 1]);
+                dp[i][j+ 1] = len;
+            }
+        }
+        return dp[0][0];
     }
 };
