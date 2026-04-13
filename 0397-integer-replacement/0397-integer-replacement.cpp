b253218b9ba3164a67093326dfccef69028c186b
sumit-codes4123 <sumitgarg4123@gmail.com>
2026-04-13 23:31:59 +0530

    Time: 1 ms (30.56%), Space: 11.3 MB (12.26%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    int cnt=0;
+    unordered_map<long long, int> memo;
     int  solve(long long  n){
         if(n==1)return 0;
-        if(n%2==0)return 1+solve(n/2);
-        else return 1+min(solve(n-1),solve(n+1));
+        if (memo.find(n) != memo.end()) return memo[n];
+        if(n%2==0)return memo[n]=1+solve(n/2);
+        else return memo[n]= 1+min(solve(n-1),solve(n+1));
     }
     int integerReplacement(int n) {
         return solve((long long)n);
