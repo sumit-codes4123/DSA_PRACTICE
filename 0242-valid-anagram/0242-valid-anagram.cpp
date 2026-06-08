4d121d15f1c81d775b36979ed49a0d1830744f5a
sumit-codes4123 <sumitgarg4123@gmail.com>
2026-06-08 17:13:08 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
-
         vector<int> res(26, 0);
-
         for (int i = 0; i < s.size(); i++) {
-            res[s[i] - 'a']++;   // increment
-            res[t[i] - 'a']--;   // decrement
+            res[s[i] - 'a']++;   
+            res[t[i] - 'a']--;  
         }
-
         for (int i = 0; i < 26; i++) {
-            if (res[i] != 0)
-                return false;
+            if (res[i] != 0)return false;
         }
-
         return true;
     }
 };
