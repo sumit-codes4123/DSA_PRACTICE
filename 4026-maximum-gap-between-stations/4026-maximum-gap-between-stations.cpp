4bda66fded8e052af66c5dc76f701704d8f659c3
Sumit Garg <sumitgarg4123@gmail.com>
2026-08-17 18:27:46 +0530

    Time: 43 ms (90.03%), Space: 72.3 MB (28.98%) - LeetHub

---
+++ 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumGap(string skill, string station) {
+        int n=skill.size();
+        int m=station.size();
+        vector<int>left(n,0),right(n,0);
+        int j=0;
+        for(int i=0;i<n;i++ ){
+            while(skill[i]!=station[j])j++;
+            left[i]=j;
+            j++;
+        }
+        j=m-1;
+        for(int i=n-1;i>=0;i--){
+            while(skill[i]!=station[j])j--;
+            right[i]=j;
+            j--;
+        }
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            ans=max(ans,right[i+1]-left[i]);
+        }
+        return ans;
+    }
+};
