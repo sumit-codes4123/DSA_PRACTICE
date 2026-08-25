70a06126c87e70791334052eb6894e72c767c795
Sumit Garg <sumitgarg4123@gmail.com>
2026-08-25 20:46:47 +0530

    Time: 0 ms (100%), Space: 21.8 MB (79.42%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<char>r[9];
-        unordered_set<char>co[9];
-        unordered_set<char>b[9];
+        bool r[9][10]={false};
+        bool co[9][10]={false};
+        bool b[9][10]={false};
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char c=board[i][j];
                 if(c=='.')continue;
+                int num=c-'0';
                 int k=(i/3)*3+(j/3);
-                if(r[i].count(c)||co[j].count(c)||b[k].count(c))return false;
-                r[i].insert(c);
-                co[j].insert(c);
-                b[k].insert(c);
+                if(r[i][num]||co[j][num]||b[k][num])return false;
+                r[i][num]=true;
+                co[j][num]=true;
+                b[k][num]=true;
             }
         }
         return true;
