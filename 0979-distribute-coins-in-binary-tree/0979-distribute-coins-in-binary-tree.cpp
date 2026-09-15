572892970054ea9fb5ddd56884b260bccd1274c2
Sumit Garg <sumitgarg4123@gmail.com>
2026-09-15 23:07:21 +0530

    Time: 0 ms (100%), Space: 16.6 MB (29.71%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int move;
+    int dfs(TreeNode* root){
+        if(root==NULL)return 0;
+        int lc=dfs(root->left);
+        int rc=dfs(root->right);
+        move+=abs(lc)+abs(rc);
+        return (root->val-1)+lc+rc;
+
+    }
+    int distributeCoins(TreeNode* root) {
+        move=0;
+        dfs(root);
+        return move;
+    }
+};
