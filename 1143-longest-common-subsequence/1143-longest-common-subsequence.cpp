215ab69df699345819d41f7d402302f22f3217df
Sumit Garg <sumitgarg4123@gmail.com>
2026-09-17 20:14:44 +0530

    Time: 31 ms (40.27%), Space: 13 MB (84.75%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
-    int lcs(string &s1, string &s2, int m, int n) {
-        if (m == 0 || n == 0) return 0;
-        if (s1[m - 1] == s2[n - 1]) {
-            return 1 + lcs(s1, s2, m - 1, n - 1);
+    int memo[1001][1001]; // DP matrix to store results
+
+    int solve(string& s1, string& s2, int i, int j) {
+        // Base case: if either string is fully processed
+        if (i == s1.size() || j == s2.size()) {
+            return 0;
         }
-        return max(lcs(s1, s2, m, n - 1), lcs(s1, s2, m - 1, n));
+
+        // Return the cached result if already calculated
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        // Scenario 1: Characters match
+        if (s1[i] == s2[j]) {
+            return memo[i][j] = 1 + solve(s1, s2, i + 1, j + 1);
+        }
+
+        // Scenario 2: Characters do not match (explore both possibilities)
+        int skip_s1 = solve(s1, s2, i + 1, j);
+        int skip_s2 = solve(s1, s2, i, j + 1);
+
+        return memo[i][j] = max(skip_s1, skip_s2);
     }
-    int longestCommonSubsequence(string t1, string t2) {
-        return lcs(t1, t2, t1.size(), t2.size());
+
+    int longestCommonSubsequence(string text1, string text2) {
+        // Initialize memoization table with -1
+        memset(memo, -1, sizeof(memo));
+        return solve(text1, text2, 0, 0);
     }
 };
